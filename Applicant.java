857013a5e7c92efe9c20609ca4e0fb1f0686d931
samirN99 <samirnaran21@gmail.com>
2024-07-29 20:33:24 +0200

    started Part B
    added the classes and the mothods

---
+++ Applicant.java
@@ -0,0 +1,23 @@
+public class Applicant {
+
+
+    private int caoNumber;
+    private String name;
+    private String college;
+    private int leavingCertPoints;
+
+    public Applicant() {
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
