0dc49071bf773d1111e27eed4b783e1c48617f00
samirN99 <samirnaran21@gmail.com>
2024-07-30 11:36:40 +0200

    added getters and setters

--- Applicant.java
+++ Applicant.java
@@ -8,16 +8,45 @@ public class Applicant {
 
     public Applicant() {
 
+    public Applicant(int caoNumber, String name, String college, int leavingCertPoints){
+            this.caoNumber = caoNumber;
+            this.name = name;
+            this.college = college;
+            this.leavingCertPoints = leavingCertPoints;
+        }
 
+        public int getCaoNumber() {
+            return caoNumber;
+        }
 
+        public void setCaoNumber(int caoNumber) {
+            this.caoNumber = caoNumber;
+        }
 
+        public String getName() {
+            return name;
+        }
 
+        public void setName(String name) {
+            this.name = name;
+        }
 
+        public String getCollege() {
+            return college;
+        }
 
+        public void setCollege(String college) {
+            this.college = college;
+        }
 
+        public int getLeavingCertPoints() {
+            return leavingCertPoints;
+        }
 
-
-
+        public void setLeavingCertPoints(int leavingCertPoints) {
+            this.leavingCertPoints = leavingCertPoints;
+        }
 
     }
 }
+
